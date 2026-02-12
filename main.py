5cf6175e98058d7eaf275d373dd10642a763cce1
hugefibero <hugefiberoj897@gmail.com>
2026-02-12 14:18:02 +0900

    init todo app

---
+++ main.py
@@ -0,0 +1,19 @@
+todos = []
+
+def add(todo):
+    todos.append(todo)
+
+def list_todos():
+    for i, t in enumerate(todos, 1):
+        print(f"{i}. {t}")
+
+if __name__ == "__main__":
+    while True:
+        cmd = input("add/list/quit > ")
+
+        if cmd == "add":
+            add(input("todo: "))
+        elif cmd == "list":
+            list_todos()
+        elif cmd == "quit":
+            break
